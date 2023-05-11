dc27580e29c6b58bafc7a3eb29373d423e6ecca5
akpi816218 <816218@seq.org>
2023-05-11 16:23:39 -0700

    STABLE explorer only

--- akpi816218/src/index.ts
+++ akpi816218/src/index.ts
@@ -1,17 +1,26 @@
 #!/usr/bin/env node
 
+// npmjs.com/package/blessed
 import blessed from 'blessed';
 const { list, screen } = blessed;
+// builtin
 import { accessSync } from 'fs';
+// builtin
 import { constants as fsConst, readdir } from 'fs/promises';
-import { cwd } from 'process';
+// builtin
+import { cwd, stdout } from 'process';
+// npmjs.com/package/ansi-colors
 import ansiColors from 'ansi-colors';
-const { blueBright, bold, cyanBright, yellowBright } = ansiColors;
+const { blueBright, bold, cyanBright, unstyle, yellowBright } = ansiColors;
 
+// global variables
 let currentFiles: string[] = [],
 	selectedFile = 0,
 	dir = cwd();
+// initialize first-tim constants
+const firstDir = cwd();
 
+// check if file is executable
 function isExec(p: string) {
 	let r;
 	try {
@@ -23,27 +32,11 @@ function isExec(p: string) {
 	return r;
 }
 
+// create blessed screen
 const win = screen({ smartCSR: true });
-
 win.title = 'tsfm';
 
-win.key('w', upHandler);
-win.key('a', async () => {});
-win.key('s', downHandler);
-win.key('d', async () => {});
-
-win.key('up', upHandler);
-win.key('down', downHandler);
-win.key('left', async () => {});
-win.key('right', async () => {});
-
-win.key(['escape', 'q', 'C-c', 'C-q', 'C-w'], () => process.exit(0));
-
-win.on('resize', () => {
-	container.width = '100%';
-	container.height = '100%';
-});
-
+// create main container (blessed list)
 const container = list({
 	top: 'center',
 	left: 'center',
@@ -54,36 +47,92 @@ const container = list({
 });
 win.append(container);
 
+// render screen with current directory
+renderDir();
+
+// key event handlers
+win.key('w', upHandler);
+win.key('a', leftHandler);
+win.key('s', downHandler);
+win.key('d', rightHandler);
+win.key('up', upHandler);
+win.key('down', downHandler);
+win.key('left', leftHandler);
+win.key('right', rightHandler);
+win.key('h', homeHandler);
+win.key('home', homeHandler);
+// resize event handler
+win.on('resize', () => {
+	container.width = '100%';
+	container.height = '100%';
+});
+// quit handlers
+win.key(['escape', 'q', 'C-c', 'C-d', 'C-q', 'C-w'], () => win.destroy());
+
+// render screen with current directory
 async function renderDir() {
+	// get current directory contents with type
 	currentFiles = (await readdir(dir, { withFileTypes: true })).map((file) => {
 		if (file.isDirectory()) return blueBright(`${file.name}/`);
 		if (file.isSymbolicLink()) return cyanBright(`${file.name}@`);
 		if (isExec(file.name)) return yellowBright(`${file.name}*`);
 		return file.name;
 	});
+	// render screen
 	await reRender();
 }
 
 async function reRender() {
+	// set container content
 	container.setItems(
 		currentFiles.map((file, i) =>
-			i === selectedFile ? bold(`> ${file}`) : `  ${file}`
+			i == selectedFile ? bold(`> ${file}`) : `  ${file}`
 		)
 	);
+	// select file for "scrolling"
 	container.select(selectedFile);
+	// render screen
 	win.render();
 }
 
+// key event handlers
 async function upHandler() {
+	// change selected file index
 	if (selectedFile > 0) selectedFile--;
 	else selectedFile = currentFiles.length - 1;
+	// re-render screen
 	await reRender();
 }
-
 async function downHandler() {
 	if (selectedFile < currentFiles.length - 1) selectedFile++;
 	else selectedFile = 0;
+	// re-render screen
 	await reRender();
 }
-
-renderDir();
+async function leftHandler() {
+	// system bell character
+	if (dir == '/') stdout.write('\u0007');
+	else {
+		dir = dir.split('/').slice(0, -1).join('/');
+		if (dir == '') dir = '/';
+		selectedFile = 0;
+		// re-render screen
+		await renderDir();
+	}
+}
+async function rightHandler() {
+	const activeDir = unstyle(currentFiles[selectedFile]);
+	if (!activeDir.endsWith('/')) stdout.write('\u0007');
+	else {
+		// change directory
+		dir += `/${activeDir.slice(0, -1)}`;
+		selectedFile = 0;
+		// re-render screen
+		await renderDir();
+	}
+}
+async function homeHandler() {
+	dir = firstDir;
+	// re-render screen
+	await renderDir();
+}
